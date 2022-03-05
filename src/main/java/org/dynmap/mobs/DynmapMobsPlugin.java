06b29fffe19220f583cb6499ae05404068d0e594
MrFawkes1337 <80531493+MrFawkes1337@users.noreply.github.com>
2022-03-05 12:59:49 +0000

    Rename Vanilla Goat to fix compat issue
    Fixes compatibility between Mo Mobs Goats and Vanilla 1.17 Goats. Renames the 1.17 Goat to vanillagoat

--- src/main/java/org/dynmap/mobs/DynmapMobsPlugin.java
+++ src/main/java/org/dynmap/mobs/DynmapMobsPlugin.java
@@ -236,7 +236,7 @@ public class DynmapMobsPlugin extends JavaPlugin {
         new MobMapping("bee", "org.bukkit.entity.Bee", "Bee" ),
         new MobMapping("strider", "org.bukkit.entity.Strider", "Strider"),
         new MobMapping("glowsquid", "org.bukkit.entity.GlowSquid", "Glow Squid"),
-        new MobMapping("goat", "org.bukkit.entity.Goat", "Goat"),
+        new MobMapping("vanillagoat", "org.bukkit.entity.Goat", "Goat"),
         new MobMapping("axolotl", "org.bukkit.entity.Axolotl", "Axolotl")
     };
 
